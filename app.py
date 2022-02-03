a92b9ccbfa040a60f840d39bea43d1ea356ce381
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-02-03 18:47:31 +0900

    add api

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import spotipy
 
+# 認証トークン
 client_id = '55cd87a12b9e429498917ac04e0412d7'
 client_secret = 'fe20d9c071684bc597d10dc2a4f4cc44'
 
+# 認証情報
 client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
@@ -16,7 +18,7 @@ def getIdByArtist(artist_name):
 
 
 def getTopSongs(artist_name):
-    num = 10
+    num = 10  # 調べたい楽曲数(最大10)
     try:
         search_id = getIdByArtist(artist_name)
         artist_top_tracks = spotify.artist_top_tracks(search_id, country="JP")["tracks"]
@@ -33,7 +35,10 @@ def getTopSongs(artist_name):
 
 
 if __name__ == '__main__':
-    getTopSongs(input("アーティストの名前を入力してください"))
-    getTopSongs(input("アーティストの名前を入力してください"))
+    getTopSongs("Mr.Children")
+    print()
+    getTopSongs("ヨルシカ")
+    print()
+    getTopSongs("oasis")
 
 
