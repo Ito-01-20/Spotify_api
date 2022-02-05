6a7258a6d92fb84b286c8ad6cd62297150c89728
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-02-05 22:18:34 +0900

    add new_function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import spotipy
-import pprint
+
 
 # 認証トークン
 client_id = '55cd87a12b9e429498917ac04e0412d7'
@@ -9,6 +9,8 @@ client_secret = 'fe20d9c071684bc597d10dc2a4f4cc44'
 client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+# show_artist = spotify.artist("0hCWVMGGQnRVfDgmhwLIxq")
+# print((show_artist))
 
 # アーティストIDの取得
 def getIdByArtist(artist_name):
@@ -19,39 +21,14 @@ def getIdByArtist(artist_name):
     return artist_id
 
 
-def getTopSongs(artist_name):
-    num = 10  # 調べたい楽曲数(最大10)
-    try:
-        search_id = getIdByArtist(artist_name)
-        artist_top_tracks = spotify.artist_top_tracks(search_id, country="JP")["tracks"]
-
-        print(artist_name + f" Top{num} Songs")
-
-        for i in range(num):
-            print(str(i + 1) + ". " + artist_top_tracks[i]["name"])
-
-    except IndexError:
-        print("IndexError has occurred!")
-    except AttributeError:
-        print("AttributeError has occurred!")
+result1 = getIdByArtist("King Gnu")
+result2 = getIdByArtist("Vaundy")
 
 
+def FollowersComparison(result1, result2):
+    if result1["followers"]["total"] > result2["followers"]["total"]:
+        print(result1["name"])
+    else:
+        print(result2["name"])
 
-
-# アーティストの楽曲一覧を取得する
-# results = spotify.artist_albums(artist_id, album_type='single', country='JP', limit=20)
-
-
-# artist_songs = []
-# for song in results['items'][:len(results)]:
-#     data = [
-#         '曲名：' + song['name'],
-#         '発売日：' + song['release_date'],
-#         'id：' + song['id']]
-#     artist_songs.append(data)
-# pprint.pprint(artist_songs)
-
-
-
-if __name__ == "__main__":
-    getTopSongs("King Gnu")
+FollowersComparison(result1, result2)
