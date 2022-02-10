56f91d2e598d39f8084dab52530bae782151ffda
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-02-10 18:35:30 +0900

    add func

--- app.py
+++ app.py
@@ -9,31 +9,26 @@ client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, 
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-class Artist(object):
-
-    def __init__(self, artist_name, artist_id):
-        self.artist_name = artist_name
-        self.artist_id = artist_id
-
-
-    # アーティストIDの取得
-    def getIdByArtist(artist_name):
-        results = spotify.search(q="artist:" + artist_name, type="artist")
-        items = results["artists"]["items"]
-        artist = items[0]
-        artist_id = artist["id"]
-        return artist_id
-
-    # アーティストの総フォロワーを取得
-    def get_artist_follwers(artist_name):
-        artist_id = Artist.getIdByArtist(artist_name)
-        show_artist = spotify.artist(artist_id)["followers"]["total"]
-        return show_artist
+# アーティストIDの取得
+def get_artist_id(artist_name):
+    results = spotify.search(q="artist:" + artist_name, type="artist")
+    items = results["artists"]["items"]
+    artist = items[0]
+    artist_id = artist["id"]
+    return artist_id
 
 
-# アーティストのalbumsを取得
+# アーティストの総フォロワーを取得
+def get_artist_follwers(artist_name):
+    artist_id = get_artist_id(artist_name)
+    show_artist = spotify.artist(artist_id)["followers"]["total"]
+    # return show_artist
+    print(show_artist)
+
+
+# アーティストのalbumsを全て取得
 def get_artist_albums(artist_name):
-    artist_id = Artist.getIdByArtist(artist_name)
+    artist_id = get_artist_id(artist_name)
     album_numbers = spotify.artist_albums(artist_id)["items"]
 
     print(f"==========={artist_name} Albums===========")
@@ -43,10 +38,23 @@ def get_artist_albums(artist_name):
         print(albums)
 
 
+# アルバムのtracksを取得
+def get_album_tracks(album_name):
+    album_id = get_album_id(album_name)
+    artist_tracks = spotify.album_tracks(album_id, market="JP")
+    return artist_tracks
 
+
+def get_album_id(album_name):
+    album_id = spotify.search(q="album:" + album_name, type="album", market="JP")
+    return album_id
 
 
 if __name__ == "__main__":
-   answer = Artist.get_artist_follwers("King Gnu")
+   # get_artist_follwers("King Gnu")
+   # get_artist_albums("King Gnu")
+   answer = get_album_id("Sympa")
    print(answer)
+   # result = get_artist_id("King Gnu")
+   # print(result)
 
